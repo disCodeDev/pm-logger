7d7819ca394fa6de42a2e01d2e30bd070d10afee
disCodeDev <simon@prizmanode.com>
2022-07-01 22:03:29 +0300

    Building Bot

--- cogs/PM.py
+++ cogs/PM.py
@@ -10,23 +10,38 @@ class Playmanity(commands.Cog):
     @commands.command()
     @commands.has_permissions()
     async def dcgames(self, ctx):
+        gamesemb = discord.Embed(timestamp=datetime.utcnow(), color=0x2F3136)
+
+
+
+
         await ctx.send("template command")
 
     @commands.command()
     @commands.has_permissions()
     async def dcideas(self, ctx):
+        ideasemb = discord.Embed(timestamp=datetime.utcnow(), color=0x2F3136)
+
+
+
+
         await ctx.send("template command")
 
     @commands.command()
     @commands.has_permissions()
     async def dcqna(self, ctx):
+        qnaemb = discord.Embed(timestamp=datetime.utcnow(), color=0x2F3136)
+
+
+
+
         await ctx.send("template command")
 
 
     @commands.command()
     @commands.has_permissions()
     async def dcinfo(self, ctx):
-        embed = discord.Embed(timestamp=datetime.utcnow(), color=0x4c00ff)
+        embed = discord.Embed(timestamp=datetime.utcnow(), color=0x2F3136)
         embed.set_author(name="Playmanity Security", url="https://playmanity.com", icon_url="https://media.discordapp.net/attachments/991739957410537537/992050893388271676/Logo_dark.png?width=409&height=409")
         embed.set_footer(text="Playmanity Security - 2022®")
         embed.add_field(name="<:pmdot2:992498402594127962> Introduction:", value="**Playmanity** is a new generation gamimg platform for all of the users around the world. We provide everyone free games using ads. This is not anoying at all, because the ads only play when the game is starting, loading or you exit the game. We bring hundreds of players to our platform every day, why don't you join?", inline=False)
